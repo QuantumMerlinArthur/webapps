60330d3dbda393bc100b0410036adaab7a7274ba
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-08 00:17:08 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,15 +5,18 @@ name = st.text_input("Enter your name", "")
 
 st.write(f"Hello {name}!")
 
-st.button('Click me')
+
 
 st.checkbox('I agree')
-val=st.radio('Pick one', ['cats', 'dogs','guinea pigs'])
+val=st.radio('Your favourite pet', ['cats', 'dogs','guinea pigs'])
 st.write(f"user input value: {val}")
 
 dict = {"cats":0,"dogs":1,"guinea pigs":2}
 
-st.write(f"array index: {dict[val]}")
+#st.write(f"array index: {dict[val]}")
+st.button('Save answer',key="save")
+
+
 
 # plotting example
 import matplotlib.pyplot as plt
@@ -23,7 +26,7 @@ arr = np.random.normal(1, 1, size=100)
 
 data = [23, 45, 56]
 fig, ax = plt.subplots()
-ax.bar([1,2,3], data)
+ax.bar(['cats', 'dogs','guinea pigs'], data)
 #ax.hist(arr, bins=20)
 
 st.pyplot(fig)
