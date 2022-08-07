f6f05c247a1b5bc40188dfa7b7aa8d2b69944655
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-08 00:13:25 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,9 +8,12 @@ st.write(f"Hello {name}!")
 st.button('Click me')
 
 st.checkbox('I agree')
-val=st.radio('Pick one', ['cats', 'dogs'])
-st.write(f"user input value:{val}")
+val=st.radio('Pick one', ['cats', 'dogs','guinea pigs'])
+st.write(f"user input value: {val}")
 
+dict = {"cats":0,"dogs":1,"guinea pigs":2}
+
+st.write(f"array index: {dict[val]}")
 
 # plotting example
 import matplotlib.pyplot as plt
