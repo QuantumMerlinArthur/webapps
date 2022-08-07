c2c7d2dbf0c0f97548b02d9a3b79e5c541e01912
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-08 00:21:05 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ st.write(f"Hello {name}!")
 
 
 st.checkbox('I agree')
-val=st.radio('Your favourite pet', ['cats', 'dogs','guinea pigs'])
+val=st.radio('Choose favourite pet', ['cats', 'dogs','guinea pigs'])
 st.write(f"user input value: {val}")
 
 dict = {"cats":0,"dogs":1,"guinea pigs":2}
@@ -16,7 +16,9 @@ dict = {"cats":0,"dogs":1,"guinea pigs":2}
 #st.write(f"array index: {dict[val]}")
 st.button('Save answer',key="save")
 
-
+poll=[0,0,0]
+if st.session_state.save:
+  poll[dict[val]]+=1
 
 # plotting example
 import matplotlib.pyplot as plt
@@ -26,7 +28,7 @@ arr = np.random.normal(1, 1, size=100)
 
 data = [23, 45, 56]
 fig, ax = plt.subplots()
-ax.bar(['cats', 'dogs','guinea pigs'], data)
+ax.bar(['cats', 'dogs','guinea pigs'], poll)
 #ax.hist(arr, bins=20)
 
 st.pyplot(fig)
