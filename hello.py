d917c2d1236b2315151840d6e189f666777163f2
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-07 23:47:44 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
 import streamlit as st
 import pandas as pd
-st.title("A Simple Streamlit Web App")
+st.title("18 questions")
 name = st.text_input("Enter your name", "")
-st.write("Fello WOrld")
+
 st.write(f"Hello {name}!")
+
+val=st.button('Click me')
+st.write(f"Button value:{val}")
+st.checkbox('I agree')
+st.radio('Pick one', ['cats', 'dogs'])
+st.selectbox('Pick one', ['cats', 'dogs'])
+st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
+st.slider('Pick a number', 0, 100)
+st.select_slider('Pick a size
+
+
+
 x = st.slider("Select an integer x", 0, 10, 1)
 y = st.slider("Select an integer y", 0, 10, 1)
 df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
