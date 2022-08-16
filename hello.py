5e228ebf7f71c2f541781f57a4aadcdc51b627fb
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-16 04:32:25 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,7 +15,7 @@ conn = connect()
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
 def run_query(query):
-    rows = conn.execute(query, headers=1)
+    rows = conn.execute(query, headers=2)
     rows = rows.fetchall()
     return rows
 
