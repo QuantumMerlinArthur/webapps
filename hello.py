5274c0433671034df06b6caf4daaf72668a0bc3a
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-16 04:09:23 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as nps
 
+####### import google sheet 
+
+import streamlit as st
+from gsheetsdb import connect
+
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+st.write(rows[0])
+
 ##### main page
 
 
