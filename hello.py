448c413a049e6753a41c43d536b34275d97431cd
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-16 04:45:22 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -23,13 +23,32 @@ sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 ############# questions and stuff
+row.'1990 lebten 58% der Weltbevölkerung in Ländern mit niedrigen Einkommen. Wo liegt dieser Anteil heute?',
+       ' Welchen Anteil haben Fischeri, Land- und Forstwirtschaft and der Weltwirtschaft?',
+       'In wie vielen Ländern gelten Gesetze gegen sexuelle Belästigung?',
+       'Welcher Anteil der Welbevölkerung lebt in Megacitys (mehr als 10 Millionen Menschen)',
+       'Welcher Anteil der Erderwärmung wird von den Ozeanen aufgenommen. (Wärmeenergie, nicht CO2)',
+       'Welcher Anteil des weltweiten Plastimülls landet in den Ozeanen?',
+       'Wie viele der 140.000 evaluerten Tier- und Pflanzenarten sind gefährdet oder bedroht?',
+       'Wie viele Menschen auf der Welt sind international auf der Flucht? (Keine Binnenflüchtlinge)',
+       'Einkommensstarke Länder bekommen welchen Anteil ihrer Einkünfte aus Zöllen und Importabgaben?',
+       'Eight billion people in the world today. Which map best shows where they live? (Each figure:  1 billion people.)',
+       'Wie hat sich der gesamte, jährliche Rohstoffverbrauch der Welt seit 2000 geändert?',
+       'Welche Ländern haben in den letzten 5 Jahren die meisten Truppen für die UN-Friedenstruppen (Blauhelme) gestellt?',
+       'Welcher Anteil der Bevölkerung der Länder mit hohem Einkommen (wie Deutschland, USA) lebt in extremer Armut ( <2$/Tag)',
+       'Welcher Anteil der Weltbevölkerung hat nicht genug  Essen, um den täglichen Bedarf zu decken? ',
+       'Wie hat sich die globale Suizidrate in den letzten 20 Jahren entwickelt? ',
+       'Wie viele Unternehmen weltweit werden von einer Frau als Top-Managerin oder CEO geleitet? ',
+       'Wie viele Mädchen gingen mind. bis zum Alter von 11 Jahren in einkommensschwachen Ländern zur Schule? (vor Corona)',
+       'Wie viele Menschen haben Zugang zu sauberem Trinkwasser in oder nahe ihres Zuhauses?',
+       'Welchen Anteil haben Kohle, Öl und Gas am gesamten Energieverbrauch der Welt?']
 ##### main page
 st.title("19 questions")
 
 #st.write([ [  row.f  for row in rows] for f in fragen  ])
 
 for row in rows:
-    st.write(row["Zeitstempel"])
+    st.write(row.'1990 lebten 58% der Weltbevölkerung in Ländern mit niedrigen Einkommen. Wo liegt dieser Anteil heute?')
     #st.write(type(row))
 
 #add name and study subject
@@ -51,7 +70,7 @@ poll=np.arange(0.0,5.0,1.0)
 if st.session_state.save:
   
   poll[dict[val]]+=1
-  np.savetxt("poll.txt",poll)
+  
   st.write(">>Saved")
 
 # plotting example
