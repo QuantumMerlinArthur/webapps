fb58b55ee32b36dc7682b9a1716da4e31853398e
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-16 04:29:52 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -47,7 +47,7 @@ fragen=['1990 lebten 58% der Weltbevölkerung in Ländern mit niedrigen Einkomme
 ##### main page
 st.title("18 questions")
 
-st.write([ [  row.f  for row in rows] for f in fragen  ])
+#st.write([ [  row.f  for row in rows] for f in fragen  ])
 
 for row in rows:
     st.write(row.Zeitstempel)
