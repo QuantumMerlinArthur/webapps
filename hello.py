4bc9c0576862b8a040b519de8931e67775348490
Arthur <110790194+QuantumMerlinArthur@users.noreply.github.com>
2022-08-16 04:37:26 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -50,7 +50,7 @@ st.title("19 questions")
 #st.write([ [  row.f  for row in rows] for f in fragen  ])
 
 for row in rows:
-    st.write(row.Zeitstempel)
+    st.write(rows.Zeitstempel)
     #st.write(type(row))
 
 #add name and study subject
